47c1c87827d27b47f85d62f4fe8ec5d546cf5d5c
Koko <104513437+kokopangeran@users.noreply.github.com>
2022-07-01 11:21:05 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -20,7 +20,7 @@ module.exports = {
         if (!m) return
         
         global.namabot = await this.user.name
-        global.wm = 'ɴᴀ፝֟፝֟uʀᴀɴ×፝֟͜×' + ' ❤️ ᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
+        global.wm = 'ɴᴀ፝֟፝֟uʀᴀɴ×፝֟͜×' + ' ♡ ᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×'
         //console.log(JSON.stringify(m, null, 4))
         try {
             m = simple.smsg(this, m) || m
