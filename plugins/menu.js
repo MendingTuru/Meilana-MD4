7ba1cafb32f0eb415535eb39d7b290348f3446db
Koko <104513437+kokopangeran@users.noreply.github.com>
2022-07-02 03:06:09 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -278,6 +278,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    let message = await prepareWAMessageMedia({ image: fs.readFileSync('./media/yuta.jpg')}, { upload: conn.waUploadToServer })
     await conn.send3TemplateButtonImg(m.chat, fla + teks, text.trim(), wm, `🏅Owner`, `${_p}owner`, `🎖ThanksTo`, `${_p}tqto`, `🎗  Donasi  🎗`, `${_p}infobot`)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
