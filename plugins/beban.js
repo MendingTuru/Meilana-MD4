f7dde93f197126225db0a660fe0a8464bb7b1199
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-02 07:40:07 +0700

    Create beban.js

---
+++ plugins/beban.js
@@ -0,0 +1,10 @@
+let handler = async m => {
+
+let Beban = `Sampai Jumpa Beban`
+m.reply(`
+_*Akhirnya Beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
+`.trim()) // Tambah sendiri kalo mau
+}
+handler.command = /^(beban)$/i
+
+module.exports = handler
