afa529b5410b1a7241af9af914be414c4ee16571
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-03 23:40:07 +0700

    Create o-tagall.js

---
+++ plugins/o-tagall.js
@@ -0,0 +1,17 @@
+let handler = async(m, { conn, text, participants }) => {
+  let teks = ` *⊙─〔 👥TAGALL👥 〕─⊙*
+\n *${text ? text : 'Nothing'}*\n\n`
+		      	for (let mem of participants) {
+		            teks += ` 👤 @${mem.id.split('@')[0]}\n`
+				}
+                teks += `\n↻ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×↻`
+                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['otagall <pesan>']
+handler.tags = ['owner']
+handler.command = /^(otagall|otagal|otall|ot)$/i
+
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+module.exports = handler
