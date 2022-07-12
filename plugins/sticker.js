edea0a10e7bee95e43eb31529628e063b0a618cd
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-12 12:57:39 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -6,33 +6,36 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (/webp|image|video/g.test(mime)) {
-      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+    if (/webp/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
-      let out
-      try {
-        if (/webp/g.test(mime)) out = await webp2png(img)
-        else if (/image/g.test(mime)) out = await uploadImage(img)
-        else if (/video/g.test(mime)) out = await uploadFile(img)
-        if (!isUrl(out)) out = await uploadImage(img)
-        stiker = await sticker(false, out, global.packname, global.author)
-      } catch (e) {
-        console.error(e)
-        if (!stiker) stiker = await sticker(img, false, global.packname, global.author)
-      }
+      if (!img) throw `balas stiker dengan perintah ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: false,
+      })
+    } else if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw `balas gambar dengan perintah ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: false,
+      })
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
+      let img = await q.download()
+      if (!img) throw `balas video dengan perintah ${usedPrefix + command}`
+      wsf = new WSF.Sticker(img, {
+        pack: global.packname,
+        author: global.author,
+        crop: true,
+      })
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('URL tidak valid!')
+      else throw 'URL tidak valid!'
     }
-  } catch (e) {
-    console.error(e)
-    if (!stiker) stiker = e
-  } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    else throw 'Conversion failed'
-  }
-}
+  } 
 handler.help = ['stiker', 'stiker <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
