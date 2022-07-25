01cd8abea3c958f0e1c78dcd999867b045f7ebdb
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-25 13:27:25 +0700

    Update infopangeran.js

--- plugins/infopangeran.js
+++ plugins/infopangeran.js
@@ -5,11 +5,13 @@ conn.reply(m.chat, `
 │ ✎ _Sekolah_ : Sudah Lulus
 │ ✎ _Umur_ : 20th
 │ ✎ _Asal_ : DKI Jakarta
-│ ✎ _Status_ : Jadi Pelampiasan
-│ ✎ _Youtube_ :
+│ ✎ _Status_ : Cowoknya Meilana♡
+│ ✎ _YouTube_ :
 │    https://youtube.com/channel/UC_nKNU3Htf4Bp_wkhj3pVXQ
-│ ✎ _Instagram_ : 
+│ ✎ _Instagram B_ : 
 │    instagram.com/kokopangeran_
+│ ✎ _Instagram G_ : 
+│    instagram.com/xrnaaazy_
 │ ✎ _WhatsApp_ :
 │    wa.me/6287832147584
 ╰───────────────────
