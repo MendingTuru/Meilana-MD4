ec5f851373229dd6c8dec423d492dec7e4141dc5
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-27 21:39:17 +0700

    Update beban.js

--- plugins/beban.js
+++ plugins/beban.js
@@ -1,10 +1,11 @@
 let handler = async m => {
 
-let Beban = `Sampai Jumpa Beban`
+let beban = `Sampai Jumpa Beban`
 m.reply(`
 _*Akhirnya Beban Keluarga Out Juga, Tolong Bilangin Ke Yang Tadi Keluar Nitip Kondom Buat Semua Orang Group Sini💦*_
 `.trim()) // Tambah sendiri kalo mau
 }
-handler.command = /^(beban)$/i
+handler.customPrefix = /beban|Beban/i
+handler.command = new RegExp
 
 module.exports = handler
