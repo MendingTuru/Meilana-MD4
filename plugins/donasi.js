5814c6aa3db4a6e442ff9b4aa02d9cff0cbfb333
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-08 02:44:07 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku
 │ • Dana  [087832147584]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? wa.me/+79811950242
+│ > Ingin donasi? wa.me/+6285929970260
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
