bf8d397d6bb27bbd0bbdf95830f1278449f6628c
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-16 11:34:19 +0700

    Update __allfake.js

--- plugins/__allfake.js
+++ plugins/__allfake.js
@@ -22,7 +22,7 @@ handler.all = async function (m) {
 		// Ini untuk command crator/owner
 		global.kontak2 = [
          [owner[0], await this.getName(owner[0] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'pangeran.dwnta01@gmail.com', true],
-         [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'pangeran.dwnta01@gmail.com', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
+         [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'ɴᴏᴍᴏʀ ʙᴏᴛ ᴡᴀ', 'pangeran.dwnta01@gmail.com', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
         ]
         
 		// ucapan ini mah
