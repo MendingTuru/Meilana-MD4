535bb7f4151167be5c647d44ae8940097af35e38
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-08-16 11:08:46 +0700

    Create main-pangerangc.js

---
+++ plugins/main-pangerangc.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+╰┈➤ ❝ *GC BOT* ❞
+✤ *ʙᴏᴛ ʙʏ ᴘᴀɴɢᴇʀᴀɴ ᴍᴅ*
+https://chat.whatsapp.com/JOTpyCQIWcC6wr17Ki7L0n
+(ɴ) sᴇʙᴇʟᴜᴍ ᴊᴏɪɴ ᴜsᴀʜᴀɪɴ ʙᴀᴄᴀ ᴅᴇsᴋ ᴅᴜʟᴜ ʏᴀ, ᴛʜᴀɴᴋʏᴏᴜᴜ
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['pangerangc']
+handler.tags = ['main']
+handler.command = /^pangeran(gc|group)$/i
+
+module.exports = handler
